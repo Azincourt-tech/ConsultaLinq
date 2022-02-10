4fff1f5dfc2f0f61416d4fa6c3ac3df82b109bb6
Willian da Silva Almeida <williandasilvaalmeida3@gmail.com>
2022-02-10 15:40:46 -0300

    Criando a classe produto e categoria.

--- Program.cs
+++ Program.cs
@@ -11,6 +11,25 @@ namespace ConsultaLinq
         static void Main(string[] args)
         {
 
+
+
+        }
+
+        class Produto
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+            public bool Status { get; set; }
+            public decimal Valor { get; set; }
+            public int CategoriaId { get; set; }
+            public List<Categoria> Categorias { get; set; }
+        }
+
+        class Categoria
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+            public bool Status { get; set; }
         }
     }
 }
