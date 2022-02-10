c03c57bc63bcfbdd47a921ff6e713f2b9902dbf7
Willian da Silva Almeida <williandasilvaalmeida3@gmail.com>
2022-02-10 15:43:07 -0300

    Adicionando fonte de dados

--- Program.cs
+++ Program.cs
@@ -11,7 +11,19 @@ namespace ConsultaLinq
         static void Main(string[] args)
         {
 
-
+            // FONTE DE DADOS
+            var listaProdutos = new List<Produto>()
+            {
+                new Produto {Id = 9, CategoriaId = 2, Nome = "Geladeira", Status = true, Valor = 10},
+                new Produto {Id = 2, CategoriaId = 3, Nome = "Short", Status = true, Valor = 1},
+                new Produto {Id = 4, CategoriaId = 1, Nome = "Maquina de lavar", Status = false, Valor = 32},
+                new Produto {Id = 3, CategoriaId = 1, Nome = "Video Game", Status = true, Valor = 99},
+                new Produto {Id = 6, CategoriaId = 2, Nome = "Arroz", Status = true, Valor = 55},
+                new Produto {Id = 8, CategoriaId = 1, Nome = "TV", Status = true, Valor = 45},
+                new Produto {Id = 1, CategoriaId = 3, Nome = "Camiseta", Status = true, Valor = 100},
+                new Produto {Id = 5, CategoriaId = 1, Nome = "Microondas", Status = true, Valor = 90},
+                new Produto {Id = 7, CategoriaId = 2, Nome = "Feijão", Status = true, Valor = 12},
+            };
 
         }
 
